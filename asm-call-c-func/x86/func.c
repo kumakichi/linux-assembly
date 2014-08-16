e0c245cfa9fcbb580b3912df30706b32bb4cd6dd
san <liebytrans@gmail.com>
2014-08-16 23:02:08 +0800

    x86 x64

---
+++ asm-call-c-func/x86/func.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int c_function(void)
+{
+	printf("Void function called from C...\n");
+	return 2;
+}
---
+++ c-call-asm-func/x86/demo2.c
@@ -0,0 +1,5 @@
+void main(void)
+{
+    asm_show_str();
+    asm_exit();
+}
